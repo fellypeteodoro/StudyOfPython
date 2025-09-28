b537dc73c4aec3747606657bf11360920ca64019
Fellype Teodoro <fellypeteodoro.dev@gmail.com>
2025-09-28 20:44:23 -0300

    Create exercicio1.py

---
+++ Atividades-s3/exercicio1.py
@@ -0,0 +1,10 @@
+"""
+Exercicio 1
+Faça um programa que leia um número inteiro e imprima-o
+
+"""
+
+nome = input("Qual é o seu nome? ")
+numero : int = int(input('Escolha um número inteiro: '))
+
+print(f"O número de {nome} é {numero}")
